2b167f11f77cdb243a529e1c3276c846579a1fb7
Nidal <Nidal@IDS.local>
2019-04-07 20:25:12 +0300

    mirage

--- IconEntry/IconEntry/IconEntry/Page1.cs
+++ IconEntry/IconEntry/IconEntry/Page1.cs
@@ -41,7 +41,7 @@ namespace IconEntry
                 else
                 {
                     Email.IsNormalFeildsSuccces = false;
-                    Email.SetWarining("Invlid Email Address test mirage");
+                    Email.SetWarining("Invlid Email Address test mirage1");
                 }
 
             }
